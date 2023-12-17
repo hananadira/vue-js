7b9a5a35d9477a0fa6e2b4cc82afc31a2b1ec73f
hananadirsav <hananadira140107@gmail.com>
2023-12-17 12:27:53 +0700

    vue-js_PT.WANTEK

---
+++ src/pages/Home.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+      <Beranda :data="DataHome"></Beranda>
+      <Service :data="DataLayanan"></Service>
+      <Portofolio :data="DataPortofolio"></Portofolio>
+      <Blog :data="DataBlog"></Blog>
+    </div>
+  </template>
+
+  <script>
+  import Beranda from '@/components/beranda/Beranda.vue';
+  import Service from '@/components/beranda/Service.vue';
+  import Portofolio from '@/components/beranda/Portofolio.vue';
+  import Blog from '@/components/beranda/Blog.vue';
+  import { Get } from '@/Api/index.js';
+  export default {
+    components: {
+      Beranda,
+      Service,
+      Portofolio,
+      Blog
+    },
+    data() {
+        return {
+            DataHome: [],
+            DataLayanan: [],
+            DataPortofolio: [],
+            DataBlog: []
+        }
+    },
+    async mounted() {
+        const response = await Get('http://localhost:9000/api/home');
+        this.DataHome = response.data;
+        const responseLayanan = await Get('http://localhost:9000/api/services');
+        this.DataLayanan = responseLayanan.data.data;
+        const responsePortofolio = await Get('http://localhost:9000/api/portfolio');
+        this.DataPortofolio = responsePortofolio.data.data;
+        const responseBlog = await Get('http://localhost:9000/api/blog');
+        this.DataBlog = responseBlog.data.data;
+    },
+  }
+  </script>
